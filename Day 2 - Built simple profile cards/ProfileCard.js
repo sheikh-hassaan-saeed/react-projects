2caeba6663a2fd19e624f98bca0b7f7e0d7c43a9
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-07-30 20:28:37 +0500

    Create ProfileCard.js

---
+++ Day 2 - Built simple profile cards/ProfileCard.js
@@ -0,0 +1,32 @@
+function ProfileCard({ name, role, department }) {
+
+    const style = {
+        card: {
+            width: 200,
+            height: 100,
+            margin: "80px auto",
+            backgroundColor: "#ffffff",
+            borderRadius: 10,
+            border: "1px solid #e5e7eb",
+            padding: 20,
+
+        },
+        name: { margin: "10 15", fontSize: 20, fontWeight: 700 },
+        role: { margin: 0, fontSize: 14, color: "#6b7280" },
+        department: { margin: 0, fontSize: 14, color: "#6b7280" },
+    }
+
+    return (
+        <div style={style.card}>
+            <h2 style={style.name}>{name}</h2>
+            <p style={style.role}>{role}</p>
+            <p style={style.department}>{department}</p>
+        </div>
+
+    );
+
+
+
+}
+
+export default ProfileCard;
