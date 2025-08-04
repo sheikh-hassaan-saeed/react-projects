f5d5465d5e4ab1a248f04dffb3c6425ef59ab1e3
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-08-04 06:09:48 +0500

    Create App.js

---
+++ Day - 7 - ToggleApp hide-show/App.js
@@ -0,0 +1,15 @@
+import ToggleApp from "./ToggleApp";
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="center">
+      <h1>Toggle App</h1>
+      <ToggleApp />
+    </div>
+  )
+
+};
+
+export default App;
