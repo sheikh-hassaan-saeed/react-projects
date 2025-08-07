b01d070a309cffd18e6c4cd7abe3e754027336e6
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-08-07 11:06:41 +0500

    Create ConditionalLogic.js

---
+++ Day 10 - Conditional-logic/ConditionalLogic.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function ConditionalToggle() {
+
+    const [loggenIn, setLoggedIn] = useState(true);
+
+
+    const BtnStyle = {
+        padding: "10px 30px",
+        backgroundColor: "#65fe08",
+        fontSize: "20px",
+        borderRadius: "6px",
+    };
+
+
+    return (
+
+        <>
+
+            {loggenIn ? <h2 style={BtnStyle}>Welcome</h2> : <button style={BtnStyle}>Log In</button>}
+
+
+        </>
+
+    )
+
+}
+
+export default ConditionalToggle;
