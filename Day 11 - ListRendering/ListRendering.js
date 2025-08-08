16c0e6350c9162c6e535abf2ca5040e54a674df0
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-08-08 18:05:19 +0500

    Create ListRendering.js

---
+++ Day 11 - ListRendering/ListRendering.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function ListRendering() {
+
+    const [products] = useState([
+        { id: 1, name: "Laptop", price: 1200 },
+        { id: 2, name: "Headphones", price: 200 },
+        { id: 3, name: "Keyboard", price: 100 }
+    ]);
+
+
+    return (
+
+        <div style={{ textAlign: "center" }}>
+
+            <h1>Available Products</h1>
+            {products.map((product) => {
+                return (
+
+                    <div key={product.id} style={{ border: "2px solid black", padding: "8px", margin: "20px" }}>
+                        <h2>{product.name}</h2>
+                        <p>{product.price}</p>
+
+                    </div>
+                )
+            })}
+
+        </div>
+
+    )
+
+
+
+};
+
+export default ListRendering;
