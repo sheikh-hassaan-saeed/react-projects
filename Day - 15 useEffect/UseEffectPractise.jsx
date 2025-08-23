2ad9d40d7fec2530b3beb405ff119040b9387a8a
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-08-23 16:18:23 +0500

    Update UseEffectPractise.jsx

--- Day - 15 useEffect/UseEffectPractise.jsx
+++ Day - 15 useEffect/UseEffectPractise.jsx
@@ -1,1 +1,21 @@
+import { useEffect, useState } from "react";
+
+function PractiseSideEffects() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCount(count + 1)
+        }, 1000);
+
+    },);
+
+    return (
+        <>
+            <h1>Counter : {count}</h1>
+        </>
+    );
+}
+
+export default PractiseSideEffects;
 
