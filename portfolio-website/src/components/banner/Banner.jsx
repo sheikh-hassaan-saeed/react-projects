a514964e255bb2041e7fb5f847ea8217ff616248
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-09-27 12:29:09 +0500

    navbar update

---
+++ portfolio-website/src/components/banner/Banner.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Banner.css'
+import linkedin from '../../assets/linkedin.png'
+import github from '../../assets/github.png'
+import circle from '../../assets/circle.png'
+
+const Banner = () => {
+    return (
+        <div className="portfolio-banner">
+
+            <div className="banner-img">
+                <img src={circle} />
+            </div>
+            <div className="main-text-area">
+                <div className="text-area">
+                    <p>Hello, I'm</p>
+                    <h1>Sheikh Hassaan</h1>
+                    <p className='text-p2'>Frontend Developer</p>
+                </div>
+
+                <div className="buttons">
+                    <button>Download CV</button>
+                    <button>Contact Info</button>
+                </div>
+
+
+                <div className="social-links">
+                    <img src={linkedin} className='icon' />
+                    <img src={github} className='icon' />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Banner
---
+++ portfolio-website/src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+const Navbar = () => {
+
+    const [isOpen, setisOpen] = useState(false);
+    return (
+        <>
+            <div className="navbar-portfolio">
+                <h2>Hassaan.</h2>
+                <div
+                    className="hamburger"
+                    onClick={() => setisOpen(!isOpen)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+                    <a href='#'>Home</a>
+                    <a href='#'>Experience</a>
+                    <a href='#'>Projects</a>
+                    <a href='#'>Contact</a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
