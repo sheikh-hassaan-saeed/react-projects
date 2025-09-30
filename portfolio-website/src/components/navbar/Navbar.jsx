f63057aece3599c008fd13c288f19464a271468d
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-09-30 20:16:28 +0500

    add scroll effect

--- portfolio-website/src/components/navbar/Navbar.jsx
+++ portfolio-website/src/components/navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Navbar.css'
+import { Link } from 'react-scroll'
 const Navbar = () => {
 
     const [isOpen, setisOpen] = useState(false);
+    const menuref = useRef(null)
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuref.current && !menuref.current.contains(event.target)) {
+                setisOpen(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [])
+
+
     return (
         <>
-            <div className="navbar-portfolio">
+            <div className="navbar-portfolio" ref={menuref}>
                 <h2>Hassaan.</h2>
                 <div
                     className="hamburger"
@@ -17,10 +34,10 @@ const Navbar = () => {
                 </div>
 
                 <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-                    <a href='#home'>Home</a>
-                    <a href='#skills'>Skills</a>
-                    <a href='#projects'>Projects</a>
-                    <a href='#contact'>Contact</a>
+                    <Link to="/" smooth={true} duration={500} onClick={() => setisOpen(false)}>Home</Link>
+                    <Link to="skills" smooth={true} offset={-130} duration={500} onClick={() => setisOpen(false)}>Skills</Link>
+                    <Link to="projects" smooth={true} offset={-50} duration={500} onClick={() => setisOpen(false)}>Projects</Link>
+                    <Link to="contact" smooth={true} duration={500} onClick={() => setisOpen(false)}>Contact</Link>
                 </div>
             </div>
         </>
