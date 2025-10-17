b6d5a9444b8eb737a8a1a3ae744164138396b625
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-10-17 18:13:11 +0500

    folder add

--- empleesy/src/App.js
+++ empleesy/src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hello world!
-      </h1>
-    </div>
+
+    <>
+
+
+    </>
   );
 }
 
---
+++ empleesy/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // React files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
