6089166099037c6209d2af94d9c92aea68a8dc20
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2025-11-25 22:44:33 +0500

    localstorage bug fix

--- empleesy/src/components/dashboard/LeaveSummary.jsx
+++ empleesy/src/components/dashboard/LeaveSummary.jsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from 'react'
 const LeaveSummary = ({ thisMonthLeaves = [], getAvatarColor = () => 'bg-gray-500', totalDaysThisMonth = 0 }) => {
 
     return (
+
         <>
-            {/* SECTION 3: THIS MONTH SUMMARY */}
-            <div className='bg-gradient-to-br from-slate-50 to-slate-100 border rounded-[21px] shadow-md p-6'>
+            <div className='bg-white rounded-lg shadow-md p-6'>
                 <h3 className='text-xl font-bold text-gray-800 mb-4 flex  items-center gap-2'>
                     <svg className='w-5 h-5 text-blue-600' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
                         <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' />
